0dac3b3c62054ee317cf997ba0eddb95e415efc9
Ricki Peikes <rickipeikes@gmail.com>
2020-11-13 11:01:02 -0500

    set up frame constructor

--- src/main/java/spof/GraphFrame.java
+++ src/main/java/spof/GraphFrame.java
@@ -3,4 +3,10 @@ package spof;
 import javax.swing.*;
 
 public class GraphFrame extends JFrame {
+    GraphView view;
+
+    public GraphFrame(){
+        setSize(view.WIDTH, view.HEIGHT);
+        add(view);
+    }
 }
