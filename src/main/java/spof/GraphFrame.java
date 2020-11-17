df1a9ce09a2dc66917912daf37ea29f4cf165b61
Ricki Peikes <rickipeikes@gmail.com>
2020-11-17 09:14:02 -0500

    removed unnecessary space from frame

--- src/main/java/spof/GraphFrame.java
+++ src/main/java/spof/GraphFrame.java
@@ -5,15 +5,13 @@ import java.awt.*;
 
 public class GraphFrame extends JFrame {
     private GraphView view;
-    public static final int MARGIN = 100;
-
 
 
     public GraphFrame(GraphView view) {
         this.view = view;
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("SPOF Identifier");
-        setSize(view.WIDTH + MARGIN, view.HEIGHT + MARGIN);
+        setSize(view.WIDTH, view.HEIGHT);
         add(view);
         view.repaint();
     }
